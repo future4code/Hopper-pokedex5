d57372c2e8b3068f7e2321c3dbc70f86c477e18e
ellen-ls <ellen.l.s1996@gmail.com>
2022-07-08 23:48:44 -0300

    teste

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -2,41 +2,35 @@ import './App.css';
 import Home from './pages/Home';
 import Details from './pages/Details';
 import Pokedex from './pages/Pokedex';
-import {BrowserRouter , Routes, Route } from 'react-router-dom';
-import {PokemonsContext} from './context/PokemonsContext'
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { PokemonsProvider } from './context/PokemonsContext'
+import { PokedexProvider } from './context/PokedexContext';
+
 
 
 
 function App() {
 
-  const [pokemons, setPokemons] = useState([])
-
-  useEffect(()=>{
-    axios.get("https://pokeapi.co/api/v2/pokemon-form/?limit=20&offset=0")
-    .then((response)=>{
-      console.log(setPokemons(response.data.results))
-    }).catch((err)=>{
-      console.log(err.response)
-
-    })
-  },[])
   return (
     <div className="App">
-      {pokemons.length&&<PokemonsContext.Provider value={pokemons}>
-      <BrowserRouter>
-      <Routes>
-      <Route path={"/"} element={<Home/>}/>
-      <Route path={"/pokedex"} element={<Pokedex/>}/>
-      <Route path={"/detail"} element={<Details/>}/>
-      </Routes>
-      </BrowserRouter>
-      </PokemonsContext.Provider>}
+      <PokemonsProvider>
+        <PokedexProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path={"/"} element={<Home />} />
+              <Route path={"/pokedex"} element={<Pokedex />} />
+              <Route path={"/:name"} element={<Details />} />
+            </Routes>
+          </BrowserRouter>
+        </PokedexProvider>
+      </PokemonsProvider>
 
 
     </div>
-  );
+
+  )
+
+
 }
 
 export default App;
